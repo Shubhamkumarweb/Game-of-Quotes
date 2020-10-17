a8c8bc4978cbec3713356258722f4cf07524d1e9
Shubham Kumar <69460704+Shubhamkumarweb@users.noreply.github.com>
2020-10-17 13:38:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,176 @@
+var data={ "quotes":[
+    {
+        "author": 'Petyr Bealish',
+        "quote": " “Money buys a man’s silence for a time. A bolt in the heart buys it forever."
+      },
+      {
+        "author": "Eddard Stark",
+        "quote": "“Can a man still be brave if he’s afraid? <br> That is the only time a man can be brave!."
+      },
+      {
+        "author": "Eddard Stark",
+        "quote": "Winter is Coming",
+            },
+      {
+        "author": "Petyr Bealish",
+        "quote": "It doesn’t matter what we want, once we get it, then we want something else."
+            },
+      {
+        "author": "Tyrion Lannister",
+        "quote": "A mind needs books like a sword needs a whetstone, if it is to keep its edge. That’s why I read so much"
+      },
+      {
+        "author": "Tyrion Lannister",
+        "quote": "I wish I was the monster you think I am. I wish I had enough poison for the whole pack of you. I would gladly give my life to watch you all swallow it. I will not give my life for Joffrey’s murder. And I know I’ll get no justice here, so I’ll let the gods decide my fate. I demand a trial by combat!"
+      },
+      {
+        "author": "Tyrion Lannister",
+        "quote": "Why is that when one man builds a wall, the next need to know what’s on the other side."
+      },
+      {
+        "author": "Jon Snow",
+        "quote": "In Battle, Discipline beats numbers 9 times out of 10."
+      },
+      {
+        "author": "Sansa Stark",
+        "quote": "You’re going to die tomorrow Lord Bolton, Sleep well."
+      },
+      {
+        "author": "Eddark(Ned) Stark",
+        "quote": "The man who passes the sentence should swing the sword.  If you would take a man's life, you owe it to him to look into his eyes and hear his final words. And if you cannot bear to do that, then perhaps the man does not deserve to die."
+      },
+      {
+        "author": "Cersie Lannister",
+        "quote": "When you play the game of thrones, you win or you die"
+      },
+      {
+        "author": "Ygritte",
+        "quote": "You know nothing, Jon Snow."
+      },
+      {
+        "author": "Aerys II Targaryen",
+        "quote": "Burn them all!"
+      },
+      {
+        "author": "Daenerys",
+        "quote": "I am the dragon's daughter, and I swear to you that those who would harm you will die screaming."
+      },
+      {
+        "author": "Varys",
+        "quote": "Power resides where men believe it resides. It's a trick; a shadow on the wall."
+      },
+      {
+        "author": "The Hound",
+        "quote": "I understand that if any more words come pouring out your c*nt mouth, I'm gonna have to eat every f*cking chicken in this room."
+      },
+      {"author": "Tywin Lannister",
+      "quote": "I would let myself be consumed by maggots before mocking the family name and making you heir to Casterly Rock."
+    },
+    {"author": "Tyrion Lannister",
+      "quote": "Oh, monster? Perhaps you should speak to me more softly then. Monsters are dangerous and just now Kings are dying like flies."
+    },
+    {"author": "Jaime Lannister",
+    "quote": "Jaime... my name's Jaime"
+  },
+  {"author": "Denerys",
+    "quote": "Lannister, Targaryen, Baratheon, Stark, Tyrell... they're all just spokes on a wheel. This one's on top, then that one's on top, and on and on it spins, crushing those on the ground"
+  },
+  {"author": "Arya Stark",
+    "quote": "When people ask you what happened here, tell them the North remembers. Tell them winter came for House Frey."
+  },
+  {"author": "Barristan Selmy",
+    "quote": "They say night’s beauties fade at dawn, and the children of wine are oft disowned in the morning light."
+  },
+  {"author": "Aemon Targaryen",
+    "quote": "We are only human, and the gods have fashioned us for love. That is our great glory, and our great tragedy."
+  },
+  {"author": "Aemon Targaryen",
+  "quote": "What is honor compared to a woman's love? What is duty against the feel of a newborn son in your arms . . . or the memory of a brother's smile? Wind and words. Wind and words. "
+},
+// something in the way
+{"author": "Brienne of Tarth",
+    "quote": "All my life men like you have sneered at me. And all my life I’ve been knocking men like you into the dust."
+  },
+  {"author": "Daenerys Targaryen",
+    "quote": "I have been sold like a broodmare. I’ve been chained and betrayed, raped and defiled. Do you know what kept me standing through all those years in exile? Faith. Not in any gods, not in myths and legends, in myself. In Daenerys Targaryen."
+  },
+  {"author": "Jon Snow",
+  "quote": "They fought together, against their common enemy. Despite their differences, despite their suspicions—together. And we need to do the same if we’re going to survive, because the enemy is real. It’s always been real."
+},
+{"author": "Daenerys Targaryen",
+    "quote": "Our fathers were evil men. All of us here. They left the world worse than they found it. We’re not going to do that. We’re going to leave the world better than we found it"
+  },
+  {"author": "Lyanna Mormont",
+    "quote": "I don’t plan on knitting by the fire while men fight for me. I might be small, Lord Glover, and I might be a girl, but I am every bit as much a northerner as you. And I don’t need your permission to defend the north."
+  },
+  {"author": "Oberyn Martell",
+    "quote": "It is a big and beautiful world. Most of us live and die in the same corner where we were born in, never get to see any of it. I don’t want to be most of us."
+  },
+  {"author": "Olenna Tyrell",
+    "quote": "I wonder if you’re the worst person I’ve ever met? At a certain age it’s hard to recall. But the truly vile do stand out through the years."
+  },
+  {"author": "Petyr Bealish",
+    "quote": "Chaos isn’t a pit. Chaos is a ladder. Many who try to climb it fail, and never get to try again. The fall breaks them. And some are given a chance to climb, but they refuse. They cling to the realm, or the gods, or love—illusions. Only the ladder is real. The climb is all there is."
+  },
+  {"author": "Aemon Targaryen",
+    "quote": "We are only human, and the gods have fashioned us for love. That is our great glory, and our great tragedy."
+  },
+  // new
+  {"author": "Samwell Tarly",
+    "quote": "That’s what death is, isn’t it? Forgetting. Being forgotten. If we forget where we’ve been and what we’ve done, we’re not men anymore. Just animals."
+  },
+  {"author": "Lord Varys",
+    "quote": "Give us common folk one taste of power and we’re like the lion who tasted man—nothing is ever so sweet again."
+  },
+  {"author": "Petyr Bealish",
+    "quote": "Chaos isn’t a pit. Chaos is a ladder. Many who try to climb it fail, and never get to try again. The fall breaks them. And some are given a chance to climb, but they refuse. They cling to the realm, or the gods, or love—illusions. Only the ladder is real. The climb is all there is."
+  },
+  {"author": "Sansa Stark",
+    "quote": "Your words will disappear. Your house will disappear. Your name will disappear. All memory of you will disappear."
+  },
+  {"author": "Tywin Lannister",
+    "quote": "Any man who must say, I am the king, is no true king."
+  },
+  {"author": "Jaime Lannister",
+    "quote": "What gods are those? The trees your husband prayed to? Where were the trees when his head was getting chopped off? If your gods are real, and if they’re just, why is the world so full of injustice?"
+  },
+  {"author": "Robert Baratheon",
+    "quote": "You want to know the horrible truth? I can't even remember what she looked like. I only know she was the one thing I ever wanted... someone took her away from me, and seven kingdoms couldn't fill the hole she left behind."
+  },
+  {"author": "Robert Baratheon",
+    "quote": "They never tell you how they all shit themselves. They don't put that part in the songs... stupid boy. Now the Tarlys bend the knee like everyone else. He could have lingered on the edge of the battle with the smart boys, and today his wife would be making him miserable, his sons would be ingrates, and he'd be waking three times in the night to piss into a bowl."
+  },
+  {"author": "Lord Varys",
+    "quote": "Power resides where men believe it resides. It’s a trick. A shadow on the wall. And a very small man can cast a very large shadow."
+  },
+  {"author": "Stannish",
+    "quote": "Lord Seaworth is a man of humble birth, but he reminded me of my duty, when all I could think of was my rights. I had the cart before the horse, Davos said. I was trying to win the throne to save the kingdom, when I should have been trying to save the kingdom to win the throne."
+  },
+  {"author": "Lord Varys",
+    "quote": "When I see what desire does to people, what it’s done to this country, I am very glad to have no part in it. Besides, the absence of desire leaves one free to pursue other things."
+  },
+  {"author": "Melisandre",
+    "quote": "It is not those foes who curse you to your face that you must fear, but those who smile when you are looking and sharpen their knives when you turn your back. You would do well to keep your wolf close beside you. Ice, I see, and daggers in the dark. Blood frozen red and hard, and naked steel. It was very cold."
+  }
+]
+};
+function randomize() {
+    var range = data.quotes.length;
+    var random_index = Math.floor(Math.random() * range);
+    var item = data.quotes[random_index];
+    console.log("Working!");
+    $('.quote-content').html(item.quote); 
+    // $('.quote-book').html(item.title);
+    $('.quote-author').html(item.author);
+  }
+  
+  function getQuote() {
+    var $button = $('.next')
+  
+    $button.on('click', randomize);
+  }
+  
+  $(document).ready(function() {
+    randomize();
+    getQuote();
+  });
